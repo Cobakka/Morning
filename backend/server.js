211fd9d128bda09ca0df6d812dd86b863a16362d
Алёна <storozevaalenka@gmail.com>
2026-04-20 17:21:05 +0500

    fix express route

--- backend/server.js
+++ backend/server.js
@@ -154,7 +154,7 @@ app.post("/api/order", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
